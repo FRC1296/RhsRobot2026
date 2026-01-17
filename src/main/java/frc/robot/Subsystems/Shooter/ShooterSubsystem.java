34da80096527e9dbfa7fdfbcca011655a2fd4595
MSI Laptop <michael.favrot@ieee.org>
2026-01-17 11:10:31 -0600

    cleanup 2

--- src/main/java/frc/robot/Subsystems/Shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterSubsystem.java
@@ -1,7 +1,100 @@
 package frc.robot.Subsystems.Shooter;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
+    
+    private TalonFX shooterHoodMotor;    // Will need to be changed to minion syntax
+    private TalonFX shooterMasterMotor;
+    private TalonFX shooterFollowerMotor;
+    private TalonFX shooterFeederMotor;
+
+    private double SHOOTER_SPEED = 0.0; // May need to change
+
+    public ShooterSubsystem(){
+        shooterHoodMotor = new TalonFX(79);
+        shooterMasterMotor = new TalonFX(69);
+        shooterFollowerMotor = new TalonFX(59);
+        shooterFeederMotor = new TalonFX(49);
+
+        TalonFXConfiguration shooterHoodConfig = new TalonFXConfiguration();
+        shooterHoodMotor.getConfigurator().apply(shooterHoodConfig);
+        TalonFXConfiguration shooterMasterConfig = new TalonFXConfiguration();
+        shooterMasterMotor.getConfigurator().apply(shooterMasterConfig);
+        TalonFXConfiguration shooterFollowerConfig = new TalonFXConfiguration();
+        shooterFollowerMotor.getConfigurator().apply(shooterFollowerConfig);
+        TalonFXConfiguration shooterFeederConfig = new TalonFXConfiguration();
+        shooterFeederMotor.getConfigurator().apply(shooterFeederConfig);
+
+        ConfigureShooterHoodMotor();
+        ConfigureShooterMasterMotor();
+        ConfigureShooterFollowerMotor();
+        ConfigureShooterFeederMotor();
+    }
+
+    public void ConfigureShooterHoodMotor(){
+        MotorOutputConfigs outputConfig =
+                new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake).withInverted(InvertedValue.Clockwise_Positive);
+
+        CurrentLimitsConfigs currentLimitConfig = new CurrentLimitsConfigs()
+                .withStatorCurrentLimitEnable(true).withStatorCurrentLimit(60); // Change later
+
+        TalonFXConfiguration shooterHoodMotorConfig = new TalonFXConfiguration()
+                .withMotorOutput(outputConfig).withCurrentLimits(currentLimitConfig);
+
+         shooterHoodMotor.getConfigurator().apply(shooterHoodMotorConfig);
+         shooterHoodMotor.setPosition(0);
+    }
+
+    public void ConfigureShooterMasterMotor(){
+        MotorOutputConfigs outputConfig =
+                new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake).withInverted(InvertedValue.Clockwise_Positive);
+
+        CurrentLimitsConfigs currentLimitConfig = new CurrentLimitsConfigs()
+                .withStatorCurrentLimitEnable(true).withStatorCurrentLimit(60); // Change later
+
+        TalonFXConfiguration shooterMasterMotorConfig = new TalonFXConfiguration()
+                .withMotorOutput(outputConfig).withCurrentLimits(currentLimitConfig);
+
+         shooterMasterMotor.getConfigurator().apply(shooterMasterMotorConfig);
+         shooterMasterMotor.setPosition(0);
+    }
+    
+    public void ConfigureShooterFollowerMotor(){
+        MotorOutputConfigs outputConfig =
+                new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake).withInverted(InvertedValue.Clockwise_Positive);
+
+        CurrentLimitsConfigs currentLimitConfig = new CurrentLimitsConfigs()
+                .withStatorCurrentLimitEnable(true).withStatorCurrentLimit(60); // Change later
+
+        TalonFXConfiguration shooterFollowerMotorConfig = new TalonFXConfiguration()
+                .withMotorOutput(outputConfig).withCurrentLimits(currentLimitConfig);
+
+         shooterFollowerMotor.getConfigurator().apply(shooterFollowerMotorConfig);
+         shooterFollowerMotor.setPosition(0);
+    }
+
+    public void ConfigureShooterFeederMotor(){
+        MotorOutputConfigs outputConfig =
+                new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake).withInverted(InvertedValue.Clockwise_Positive);
+
+        CurrentLimitsConfigs currentLimitConfig = new CurrentLimitsConfigs()
+                .withStatorCurrentLimitEnable(true).withStatorCurrentLimit(60); // Change later
+
+        TalonFXConfiguration shooterFeederMotorConfig = new TalonFXConfiguration()
+                .withMotorOutput(outputConfig).withCurrentLimits(currentLimitConfig);
+
+         shooterFeederMotor.getConfigurator().apply(shooterFeederMotorConfig);
+         shooterFeederMotor.setPosition(0);
+    }
+
+
 
 }
--- src/main/java/frc/robot/Subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/Subsystems/intake/IntakeSubsystem.java
@@ -1,7 +1,77 @@
 package frc.robot.Subsystems.intake;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase{
+    private TalonFX intakeMotor;
+    private TalonFX intakeDeployMotor;
 
+    private DutyCycleOut dcOut = new DutyCycleOut(0);
+
+    
+    // Constants undecided as of 1/16/26
+    private double INTAKE_SPEED = 0.0;
+    private double DEPLOY_SPEED = 0.0;
+
+    public IntakeSubsystem(){
+        intakeMotor = new TalonFX(99);
+        intakeDeployMotor = new TalonFX(89);
+
+
+        TalonFXConfiguration intakeConfig = new TalonFXConfiguration();
+        intakeMotor.getConfigurator().apply(intakeConfig);
+        TalonFXConfiguration intakeDeployConfig = new TalonFXConfiguration();
+        intakeDeployMotor.getConfigurator().apply(intakeDeployConfig);
+
+        ConfigureIntakeMotor();
+        ConfigureDeployMotor();
+    }
+
+    private void ConfigureIntakeMotor(){
+        
+        MotorOutputConfigs outputConfig =
+                new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Coast).withInverted(InvertedValue.Clockwise_Positive);
+
+        CurrentLimitsConfigs currentLimitConfig = new CurrentLimitsConfigs()
+                .withStatorCurrentLimitEnable(true).withStatorCurrentLimit(60); // Change later
+
+        TalonFXConfiguration intakeMotorConfig = new TalonFXConfiguration()
+                .withMotorOutput(outputConfig).withCurrentLimits(currentLimitConfig);
+
+         intakeMotor.getConfigurator().apply(intakeMotorConfig);
+         intakeMotor.setPosition(0);
+    }
+
+    private void ConfigureDeployMotor(){
+
+        MotorOutputConfigs outputConfig =
+                new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake).withInverted(InvertedValue.Clockwise_Positive);
+
+        CurrentLimitsConfigs currentLimitConfig = new CurrentLimitsConfigs()
+                .withStatorCurrentLimitEnable(true).withStatorCurrentLimit(60); // Change later
+
+        TalonFXConfiguration intakeDeployMotorConfig = new TalonFXConfiguration()
+                .withMotorOutput(outputConfig).withCurrentLimits(currentLimitConfig);
+
+         intakeMotor.getConfigurator().apply(intakeDeployMotorConfig);
+         intakeMotor.setPosition(0);
+    }
+
+     public void runIntake() {
+         intakeMotor.setControl(dcOut.withOutput(1.0));
+    }
+
+    public void stopIntake() {
+         intakeMotor.setControl(dcOut.withOutput(0.0));
+    }
 }
