a7d4c70839b6a70750e3933cab1e211225f3866e
Michael Favrot <michael.favrot@ieee.org>
2026-02-20 09:23:33 -0600

    Logging setup, PathPlanner Init, moved robot creation to robotInit method

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,96 +4,115 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.SignalLogger;
+import com.pathplanner.lib.commands.FollowPathCommand;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+    private Command m_autonomousCommand;
 
-  private final FMJRobotContainer m_robotContainer;
+    private FMJRobotContainer m_robotContainer;
 
-  public Robot() {
-    m_robotContainer = new FMJRobotContainer();
-  }
+    public Robot() {}
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-    m_robotContainer.robotPeriodic();
-  }
+    @Override
+    public void robotInit() {
+        // Create Robot
+        m_robotContainer = new FMJRobotContainer();
 
-  @Override
-  public void disabledInit() {
-    LimelightHelpers.setPipelineIndex("limelight-a", 1);
-    LimelightHelpers.setPipelineIndex("limelight-b", 1);
-    LimelightHelpers.SetThrottle("limelight-a", 200);
-    LimelightHelpers.SetThrottle("limelight-b", 200);
-  }
+        // Initialize PathPlanner
+        FollowPathCommand.warmupCommand();
 
-  @Override
-  public void disabledPeriodic() {
-  }
-
-  @Override
-  public void disabledExit() {
-  }
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+        // Start WPILib Data Log
+        DataLogManager.start(); // stores logs in either USB(diretory logs) or roborio drive(/home/lvuser/logs)
+        DriverStation.startDataLog(DataLogManager.getLog());
+        
+        // Start CTRE Data Log - logging will automatically start for FRC match
+        SignalLogger.setPath("/media/sda1/ctre-logs"); // we need to valid this location
+        SignalLogger.start();
     }
 
-    LimelightHelpers.setPipelineIndex("limelight-a", 0);
-    LimelightHelpers.setPipelineIndex("limelight-b", 0);
-    LimelightHelpers.SetThrottle("limelight-a", 0);
-    LimelightHelpers.SetThrottle("limelight-b", 0);
-  }
-
-  @Override
-  public void autonomousPeriodic() {
-  }
-
-  @Override
-  public void autonomousExit() {
-  }
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+        m_robotContainer.robotPeriodic();
     }
-    // m_robotContainer.setInitialPose(1.3, 2.4);
 
-    LimelightHelpers.setPipelineIndex("limelight-a", 0);
-    LimelightHelpers.setPipelineIndex("limelight-b", 0);
-    LimelightHelpers.SetThrottle("limelight-a", 0);
-    LimelightHelpers.SetThrottle("limelight-b", 0);
-  }
+    @Override
+    public void disabledInit() {
+        LimelightHelpers.setPipelineIndex("limelight-a", 1);
+        LimelightHelpers.setPipelineIndex("limelight-b", 1);
+        LimelightHelpers.SetThrottle("limelight-a", 200);
+        LimelightHelpers.SetThrottle("limelight-b", 200);
+    }
 
-  @Override
-  public void teleopPeriodic() {
-    m_robotContainer.teleopPeriodic();
+    @Override
+    public void disabledPeriodic() {
+    }
 
-  }
+    @Override
+    public void disabledExit() {
+    }
 
-  @Override
-  public void teleopExit() {
-  }
+    @Override
+    public void autonomousInit() {
+        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.schedule();
+        }
 
-  @Override
-  public void testPeriodic() {
-  }
+        LimelightHelpers.setPipelineIndex("limelight-a", 0);
+        LimelightHelpers.setPipelineIndex("limelight-b", 0);
+        LimelightHelpers.SetThrottle("limelight-a", 0);
+        LimelightHelpers.SetThrottle("limelight-b", 0);
+    }
 
-  @Override
-  public void testExit() {
-  }
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void autonomousExit() {
+    }
+
+    @Override
+    public void teleopInit() {
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.cancel();
+        }
+        // m_robotContainer.setInitialPose(1.3, 2.4);
+
+        LimelightHelpers.setPipelineIndex("limelight-a", 0);
+        LimelightHelpers.setPipelineIndex("limelight-b", 0);
+        LimelightHelpers.SetThrottle("limelight-a", 0);
+        LimelightHelpers.SetThrottle("limelight-b", 0);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        m_robotContainer.teleopPeriodic();
+
+    }
+
+    @Override
+    public void teleopExit() {
+    }
+
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
+
+    @Override
+    public void testExit() {
+    }
 }
